715b0e653172e6d85762ccf5fbf2b9e853a762fb
haslich-sourse <igor.mizev01@gmail.com>
2024-10-09 18:53:26 +0500

    ADD DB AND README

---
+++ portfolio/script.js
@@ -0,0 +1,18 @@
+// Бургер-меню
+
+const burgerMenu = document.querySelector('.burger-menu');
+const menu = document.querySelector('.menu');
+burgerMenu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+// Hover effect on projects
+document.querySelectorAll('.project').forEach((project) => {
+    project.addEventListener('mouseover', function() {
+        this.style.backgroundColor = '#f9f9f9';
+    });
+
+    project.addEventListener('mouseout', function() {
+        this.style.backgroundColor = '#fff';
+    });
+});
