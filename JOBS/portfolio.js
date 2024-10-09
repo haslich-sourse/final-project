ee0ebe227e9b1173aea56e5730712aa54f0d0001
haslich-sourse <igor.mizev01@gmail.com>
2024-10-09 17:19:25 +0500

    add a jobs

---
+++ JOBS/portfolio.js
@@ -0,0 +1,5 @@
+// Функция для переключения бургер-меню
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
