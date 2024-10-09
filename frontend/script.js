9cadbafecb5358f79ac1f8302f85e7e943550e45
haslich-sourse <igor.mizev01@gmail.com>
2024-10-09 16:42:17 +0500

    add func

--- frontend/script.js
+++ frontend/script.js
@@ -23,3 +23,37 @@ canvas.style.position = 'absolute';
 canvas.style.top = '0';
 canvas.style.left = '0';
 canvas.style.zIndex = '-1'; // Задаем canvas позицию на заднем плане
+
+const stars = [];
+
+// Функция для создания звезд
+function createStars() {
+    stars.length = 0; // Очищаем массив звезд при изменении размера
+    for (let i = 0; i < 100; i++) {
+        stars.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            size: Math.random() * 5,
+            speed: Math.random() * 0.5 + 0.5
+        });
+    }
+}
+
+
+function drawStars() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    stars.forEach(star => {
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
+        ctx.fillStyle = '#000';
+        ctx.fill();
+
+        star.y += star.speed;
+        if (star.y > canvas.height) {
+            star.y = 0;
+        }
+    });
+
+    requestAnimationFrame(drawStars);
+}
