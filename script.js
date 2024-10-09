3e017299d4f9cba847d1f808c51f9fa03bf82e57
haslich-sourse <igor.mizev01@gmail.com>
2024-10-09 16:16:07 +0500

    add anim

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ window.onload = function() {
         button.style.opacity = '1';
     }, 500);
 };
+
+// Бургер-меню
+
+console.log('Скрипт загружен');
+
+const burgerMenu = document.querySelector('.burger-menu');
+const menu = document.querySelector('.menu');
+burgerMenu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
